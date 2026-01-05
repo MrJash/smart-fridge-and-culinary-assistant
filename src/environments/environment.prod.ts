9b3c87d30f7ff8595fa2e8db55648fba57b65a57
MrJash <jashrmewada@gmail.com>
2026-01-05 15:05:56 +0530

    Security: Remove API keys from git, use env vars for Vercel

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  geminiApiKey: typeof process !== 'undefined' && process.env['NG_APP_GEMINI_API_KEY']
-    ? process.env['NG_APP_GEMINI_API_KEY']
-    : ''
-};
