32c03c6169805a158b4cb2e308d06abc02dbed49
MrJash <jashrmewada@gmail.com>
2026-01-05 15:14:00 +0530

    Fix: Restore environment files for build

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  geminiApiKey: 'AIzaSyCU2N6sTEzSfKk7mtG_yHRrm_rLJyjU954'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  geminiApiKey: 'AIzaSyCU2N6sTEzSfKk7mtG_yHRrm_rLJyjU954'
+};
